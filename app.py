e99c912f06a4e22c71093eea417c708a83db328f
Nicholas Femia <femianicholas@gmail.com>
2021-02-20 13:10:30 -0500

    parse through incoming data and add to relevant type history data structures

--- app.py
+++ app.py
@@ -4,19 +4,49 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-items = []
-class App(Resource):
-    
+device_status_history = []
+location_history = []
+geofence_history = []
+battery_history = []
+
+class Webhook(Resource):
     def post(self):
         data = request.json
-        items.append(data)
-        return data
+        print(data)
+        for item in data:
+            if (item['type'] == 'device_status'):
+                device_status_history.append(item)
+            elif (item['type'] == 'location'):
+                location_history.append(item)
+            elif (item['type'] == 'geofence'):
+                geofence_history.append(item)
+            elif (item['type'] == 'battery'):
+                battery_history.append(item)
+            else:
+                return {'message': 'Invalid data received'}, 400
         
-        
+        return {'message': 'data posted correctly'}, 201
+class DeviceStatus(Resource):
     def get(self):
-        return items
+        return device_status_history, 200
+
+class Location(Resource):
+    def get(self):
+        return location_history, 200
+
+class GeoFence(Resource):
+    def get(self):
+        return geofence_history, 200
+
+class Battery(Resource):
+    def get(self):
+        return battery_history, 200
     
-api.add_resource(App, '/')
+api.add_resource(Webhook, '/')
+api.add_resource(DeviceStatus, '/deviceStatus')
+api.add_resource(Location, '/location')
+api.add_resource(GeoFence, '/geofence')
+api.add_resource(Battery, '/battery')
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
