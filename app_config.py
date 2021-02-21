023bcc9a9c25d6c05907acdc58c5c31389d5026a
Nicholas Femia <femianicholas@gmail.com>
2021-02-21 12:45:28 -0500

    add CORS

--- app_config.py
+++ app_config.py
@@ -2,9 +2,14 @@ from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 
 app = Flask(__name__)
 
+cors = CORS(app)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/hyper-track'
 
 db = SQLAlchemy(app)
