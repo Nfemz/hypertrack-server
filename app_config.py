9df125c1dadad8cf0121da00143f284848bdb792
Nicholas Femia <femianicholas@gmail.com>
2021-02-21 13:11:00 -0500

    simplify CORS

--- app_config.py
+++ app_config.py
@@ -5,6 +5,7 @@ from flask_marshmallow import Marshmallow
 from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 if __name__ == '__main__':
     postgres_uri = 'postgresql://localhost/hyper-track'
@@ -12,11 +13,6 @@ else:
     postgres_uri = 'postgres://rtsxdpgdxdnmjb:e88e7ac4409a75ca38a3b3b702b45942416a56db54b1a87eaf2ace51f00c05b1@ec2-54-198-73-79.compute-1.amazonaws.com:5432/d31i7r7eemsjrq'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = postgres_uri
-
-CORS(app, resources={r"/": {"origins": "*"}})
-
-app.config['CORS_HEADERS'] = 'Content-Type'
-
 
 db = SQLAlchemy(app)
 
