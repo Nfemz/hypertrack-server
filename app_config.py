5ea2af3862c35f8525217074b0f9b15054cdcc3a
Nicholas Femia <femianicholas@gmail.com>
2021-02-21 12:50:40 -0500

    modify CORS wrapper

--- app_config.py
+++ app_config.py
@@ -7,9 +7,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 
 cors = CORS(app)
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/hyper-track'
 
 db = SQLAlchemy(app)
